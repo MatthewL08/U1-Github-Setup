c8d9db816318ef56d3613ac62087462a210d35c4
MatthewLin <matthewl439@nycstudents.net>
2025-09-22 08:43:31 -0400

    w/o rounding

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is your Bill amount($) : ");
+        double totalBill = scan.nextDouble();
+        System.out.print("What is your Tip percentage : ");
+        double tipPercent = scan.nextDouble();
+        System.out.print("Number of People : ");
+        int numOfPeople = scan.nextInt();
+
+        double tipPercentDecimal = tipPercent/100;
+        double totalTipAmount = (totalBill * tipPercentDecimal);
+        double totalBillCost = totalTipAmount + totalBill;
+        double perPersonTip =  totalTipAmount/numOfPeople;
+        double perPersonBill =  totalBillCost/numOfPeople;
+
+        System.out.println("Total tip amount: $" + totalTipAmount);
+        System.out.println("Total bill cost: $" + totalBillCost);
+        System.out.println("Tip per Person: $" + perPersonTip);
+        System.out.println("Total bill per person: $" + perPersonBill);
     }
 }

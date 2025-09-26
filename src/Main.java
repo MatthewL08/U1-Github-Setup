ca29a697c8f83d798da0caf7bd97c23e9266dded
MatthewLin <matthewl439@nycstudents.net>
2025-09-26 08:20:59 -0400

    Finish1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Do not use any symbols when imputing the data below    |");
+        System.out.println("--------------------------------------------------------");
         System.out.print("What is your Bill amount($) : ");
         double totalBill = scan.nextDouble();
         System.out.print("What is your Tip percentage : ");
@@ -19,9 +22,14 @@ class Main {
         double perPersonBill =  totalBillCost/numOfPeople;
 
         DecimalFormat df2 = new DecimalFormat("0.00");
-        System.out.println("Total tip amount: $" + df2.format(totalTipAmount));
-        System.out.println("Total bill cost: $" + df2.format(totalBillCost));
-        System.out.println("Tip per Person: $" +  df2.format(perPersonTip));
-        System.out.println("Total bill per person: $" +  df2.format(perPersonBill));
+        System.out.println("--------------------------------------------");
+        System.out.println("|Total tip amount: $" + df2.format(totalTipAmount) + "                    |");
+        System.out.println("--------------------------------------------");
+        System.out.println("|Total bill cost: $" + df2.format(totalBillCost) + "                    |");
+        System.out.println("--------------------------------------------");
+        System.out.println("|Tip per Person: $" +  df2.format(perPersonTip) + "                      |");
+        System.out.println("--------------------------------------------");
+        System.out.println("Total bill per person: $" +  df2.format(perPersonBill) + "               |");
+        System.out.println("--------------------------------------------");
     }
 }

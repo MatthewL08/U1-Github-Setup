135b8b1d711e78cbba62e7b56ae6c015fc360b15
MatthewLin <matthewl439@nycstudents.net>
2025-09-29 08:13:04 -0400

    Finish1

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,18 @@ class Main {
         System.out.println("--------------------------------------------------------");
         System.out.print("What is your Bill amount($) : ");
         double totalBill = scan.nextDouble();
+        // Ask the user for their bill amount for future use in the calculation for tip amount and price
+
         System.out.print("What is your Tip percentage : ");
         double tipPercent = scan.nextDouble();
+
+        // Ask the user for their tip amount for future use in the calculation for tip amount and price
         System.out.print("Number of People : ");
         int numOfPeople = scan.nextInt();
+        // Ask the user for the number of people in the restaurant for future use in the calculation for tip amount and price
 
         double tipPercentDecimal = tipPercent/100;
+        // changes the tip amount from a number into a decimal to correctly uses convert from a percentage
         double totalTipAmount = (totalBill * tipPercentDecimal);
         double totalBillCost = totalTipAmount + totalBill;
         double perPersonTip =  totalTipAmount/numOfPeople;
